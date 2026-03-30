f07611621ac60a6bd6836f9c713a7debd77c8796
Yug-Kapri <yugkapri.07@gmail.com>
2026-03-30 21:38:32 +0530

    updated eks config

--- EKS/main.tf
+++ EKS/main.tf
@@ -30,12 +30,18 @@ module "vpc" {
 
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
 
   name    = "my-eks-cluster"
-  kubernetes_version = "1.31"
+  kubernetes_version = "1.33"
 
   endpoint_public_access = true
 
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+  
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
